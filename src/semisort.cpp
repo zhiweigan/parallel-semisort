b524d9750ea3a66efc846f373c7a25fef28fce20
RicardoGayle <rgayle@mit.edu>
2022-03-16 10:37:51 -0400

    started semiSort algo

--- src/semisort.cpp
+++ src/semisort.cpp
@@ -1,10 +1,38 @@
 #include <stdio.h>
 #include <iostream>
+#include <functional>
 #include "../parlaylib/include/parlay/parallel.h"
+#include "../parlaylib/include/parlay/sequence.h"
 
 using namespace std;
 
-int main(){
-    cout<<"hi"<<endl;
-    return 0;
+template <class a, class b>
+struct record {
+    a obj;
+    b key;
+    int hashed_key;
+};
+
+template <class T>
+void semiSort(parlay::sequence<T> &A){
+    hash<T, int> hash_fn;
+    int param1 = 3; // TODO: this is a parameter we can change -- k > 2 from algo
+    int k = pow(A.size(), param1);
+    for (int i = 0; i < A.size(); i++) { // TODO: can parallelize
+        A[i].hashed_key = hash_fn(A[i]) % k;
+    }
+    paralay::sequence<T> sample;
+    int param2 = 1; 
+    int p = param2 / log(A.size()); // this is theta(1 / log n) so we can autotune later
+    int cp = ceil(1 / p);
+    for (int i = 0; i < p; i++) {
+        sample[i] = A[rand() % cp + i/p];
+    }
+    
 }
+
+// if what you get from hashing k is the same as what you get from hashing j
+// then it is possible k and j are equal, but not necessarily true
+
+// but if k and j are equal, then they will have the same value when you
+// hash them guaranteed
