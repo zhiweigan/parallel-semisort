e39ef432c2c90c9f56e6a153c2f15d543919593c
RicardoGayle <rgayle@mit.edu>
2022-03-17 17:25:05 -0400

    Wrote out to step 4

--- src/semisort.cpp
+++ src/semisort.cpp
@@ -22,18 +22,37 @@ void semiSort(parlay::sequence<T> &A){
     parlay::parallel_for (int i = 0; i < A.size(); i++) { // TODO: can parallelize
         A[i].hashed_key = hash_fn(A[i]) % k;
     }
+    // Create a frequency map for step 4
+    unordered_map<int, int> key_frequency;
     // Step 2
     paralay::sequence<T> sample;
     int param2 = 1; 
     int p = param2 / log(A.size()); // this is theta(1 / log n) so we can autotune later
     int cp = ceil(1 / p);
-    parlay::parallel_for (int i = 0; i < p; i++) {
+    // parlay::parallel_for (int i = 0; i < p; i++) {
+    for (int i = 0; i < p; i++) {
         sample[i] = A[rand() % cp + i/p];
+        // Update frequency map for step 4
+        if (key_frequency.find(sample[i]) == key_frequency.end()) {
+            key_frequency[sample[i]] = 0;
+        }
+        key_frequency[sample[i]]++;
     }
     // Step 3
     sample.sort(); // TODO: this needs to be radix sort from PBBS
     // Step 4
-    
+    unordered_set<int> h;
+    unordered_set<int> l;
+    int param3 = 1;
+    int gamma = param3 * log(A.size())
+    for (int key : key_frequency) {
+        if (key_frequency[key] > gamma) {
+            h.push(key);
+        } else {
+            l.push(key);
+        }
+    }
+    // Step 5
 }
 
 // if what you get from hashing k is the same as what you get from hashing j
