5989bbad78327f3e76d9df9c6cc2669ccf809113
fengqing <fengqingthu@gmail.com>
2022-03-25 01:24:39 -0400

    Got three types of distributions set up

--- benchmark/bench_semisort.cpp
+++ benchmark/bench_semisort.cpp
@@ -6,9 +6,20 @@
 #include <parlay/primitives.h>
 #include <parlay/random.h>
 #include <parlay/io.h>
-#include "trigram_words.h"
+
+#include <iostream>
+#include <random>
+
+#include <semisort/semisort.h>
+#include "genzipf.cpp"
 
 using benchmark::Counter;
+
+using parlay::parallel_for;
+
+#ifndef PARALLEL
+#define parallel_for for
+#endif
 
 // Use this macro to avoid accidentally timing the destructors
 // of the output produced by algorithms that return data
@@ -45,25 +56,119 @@ using benchmark::Counter;
   state.counters["    Elements/sec"] = Counter(state.iterations()*(n), Counter::kIsRate);                                            \
   state.counters["       Bytes/sec"] = Counter(state.iterations()*(n)*(sizeof(T)), Counter::kIsRate);
 
+const float HASH_RANGE_K = 2.25; // here should put this in the header
 
-// 
+// Return uniform_distributions input, note that the records are already hashed
+static parlay::sequence<record<uint64_t, uint64_t>> uniform_distribution_input(size_t n, size_t para) {
+  
+  std::default_random_engine generator;
+  std::uniform_int_distribution<uint64_t> distribution(0, para);
+  uint64_t k = pow(n, HASH_RANGE_K);
+  uint64_t key;
+
+  parlay::sequence<record<uint64_t, uint64_t>> arr(n);
+  parallel_for (size_t i = 0; i < n; i++) {
+    key = distribution(generator);
+    record<uint64_t, uint64_t> elt = {
+      0,
+      key,
+      static_cast<int>(parlay::hash64(key) % k) // here should change to uint64_t
+    };
+    arr[i] = elt;
+  }
+
+  return arr;
+}
+
+// Return exponential_distribution input, note that the records are already hashed
+static parlay::sequence<record<uint64_t, uint64_t>> exponential_distribution_input(size_t n, size_t para) {
+  
+  std::default_random_engine generator;
+  std::exponential_distribution<double> distribution(para);
+  uint64_t k = pow(n, HASH_RANGE_K);
+  uint64_t key;
+
+  parlay::sequence<record<uint64_t, uint64_t>> arr(n);
+  parallel_for (size_t i = 0; i < n; i++) {
+    key = static_cast<uint64_t>(n * distribution(generator));
+    record<uint64_t, uint64_t> elt = {
+      0,
+      key,
+      static_cast<int>(parlay::hash64(key) % k) // here should change to uint64_t
+    };
+    arr[i] = elt;
+  }
+
+  return arr;
+}
+
+// Return zipfian_distribution input, note that the records are already hashed
+static parlay::sequence<record<uint64_t, uint64_t>> zipfian_distribution_input(size_t n, size_t para) {
+  
+  rand_val(1);
+  uint64_t k = pow(n, HASH_RANGE_K);
+  uint64_t key;
+
+  parlay::sequence<record<uint64_t, uint64_t>> arr(n);
+  parallel_for (size_t i = 0; i < n; i++) {
+    key = static_cast<uint64_t>(zipf(1.0, para)); // according to section 5.1: "the i-th number in this range has a probability 1/(iM-) of being chosen..."
+    record<uint64_t, uint64_t> elt = {
+      0,
+      key,
+      static_cast<int>(parlay::hash64(key) % k) // here should change to uint64_t
+    };
+    arr[i] = elt;
+  }
+
+  return arr;
+}
+
+// Define the semi-sort benchmark
 template<typename T>
-static void bench_semisort(benchmark::State& state) {
+static void bench_semi_sort(benchmark::State& state) {
+  size_t n = state.range(0);
+  while (state.KeepRunningBatch(1)) {
+    std::cout<<"uniform_distribution:"<< std::endl;
+    auto arr1 = uniform_distribution_input(n, n);
+    for(size_t i = 0; i < arr1.size(); i++){
+      std::cout<<arr1[i].key<<" ";
+    }
+    std::cout<<std::endl;
+    std::cout<<"exponential_distribution:"<< std::endl;
+    auto arr2 = exponential_distribution_input(n, n/1000);
+    for(size_t i = 0; i < arr2.size(); i++){
+      std::cout<<arr2[i].key<<" ";
+    }
+    std::cout<<std::endl;
+    std::cout<<"zipfian_distribution:"<< std::endl;
+    auto arr3 = zipfian_distribution_input(n, n);
+    for(size_t i = 0; i < arr3.size(); i++){
+      std::cout<<arr3[i].key<<" ";
+    }
+    std::cout<<std::endl;
+  }
+  
+  REPORT_STATS(n, 0, 0);
+}
+
+// Define the radix-sort benchmark
+template<typename T>
+static void bench_integer_sort(benchmark::State& state) {
   size_t n = state.range(0);
   parlay::random r(0);
-  auto in = parlay::tabulate(n, [&] (size_t i) -> T {return r.ith_rand(i)%n;});
-  auto out = in;
+  size_t bits = sizeof(T)*8;
+  auto S = parlay::tabulate(n, [&] (size_t i) -> T {
+				 return r.ith_rand(i);});
+  auto identity = [] (T a) {return a;};
 
   while (state.KeepRunningBatch(10)) {
     for (int i = 0; i < 10; i++) {
-      COPY_NO_TIME(out, in);
-      parlay::internal::merge_sort_inplace(make_slice(out), std::less<T>());
+      RUN_AND_CLEAR(parlay::internal::integer_sort(parlay::make_slice(S), identity, bits));
     }
   }
 
   REPORT_STATS(n, 0, 0);
 }
-
 
 // ------------------------- Registration -------------------------------
 
@@ -73,5 +178,7 @@ static void bench_semisort(benchmark::State& state) {
                           ->Args({args});
 
 
-BENCH(semisort, long, 100000000);
+// BENCH(integer_sort, long, 1000);
+BENCH(semi_sort, size_t, 1000);
+
 
